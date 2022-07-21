5c655e1bab30f2d417aad19367b409ade125cd11
numq <numq.dev@gmail.com>
2022-07-21 15:28:49 +0300

    Provide permissions request changes

--- app/src/main/java/com/numq/stash/navigation/AppRouter.kt
+++ app/src/main/java/com/numq/stash/navigation/AppRouter.kt
@@ -1,29 +1,20 @@
 package com.numq.stash.navigation
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Close
-import androidx.compose.material.icons.rounded.Done
-import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.PermissionState
-import com.google.accompanist.permissions.isGranted
-import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.numq.stash.files.FilesScreen
-import kotlinx.coroutines.runBlocking
+import com.numq.stash.permission.PermissionsRequired
 
-@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun AppRouter() {
 
@@ -31,14 +22,12 @@ fun AppRouter() {
     val scaffoldState = rememberScaffoldState()
     val navBackStackEntry by navController.currentBackStackEntryAsState()
 
-    val permissions = rememberMultiplePermissionsState(
-        listOf(
-            android.Manifest.permission.READ_EXTERNAL_STORAGE,
-            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
-        )
+    val permissions = listOf(
+        android.Manifest.permission.READ_EXTERNAL_STORAGE,
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
     )
 
-    if (permissions.allPermissionsGranted) {
+    PermissionsRequired(permissions) {
         Scaffold(scaffoldState = scaffoldState) {
             Box(
                 Modifier
@@ -53,56 +42,6 @@ fun AppRouter() {
                         FilesScreen(scaffoldState, navController)
                     }
                 }
-            }
-        }
-    } else {
-        NoPermissions(permissions.permissions) {
-            runBlocking {
-                permissions.launchMultiplePermissionRequest()
-            }
-        }
-    }
-}
-
-@OptIn(ExperimentalPermissionsApi::class)
-@Composable
-fun NoPermissions(permissions: List<PermissionState>, grant: () -> Unit) {
-    Box(
-        Modifier
-            .fillMaxSize()
-            .padding(8.dp), contentAlignment = Alignment.Center
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text("You must grant these permissions:")
-            Spacer(Modifier.height(16.dp))
-            Divider()
-            Spacer(Modifier.height(32.dp))
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                permissions.forEach {
-                    Row(
-                        Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.Center,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Icon(
-                            if (it.status.isGranted) Icons.Rounded.Done else Icons.Rounded.Close,
-                            "",
-                            tint = if (it.status.isGranted) Color.Green else Color.Red
-                        )
-                        Text(
-                            it.permission.split(".").last(),
-                            color = if (it.status.isGranted) Color.Green else Color.Red
-                        )
-                        Spacer(modifier = Modifier.height(8.dp))
-                    }
-                }
-            }
-            Spacer(Modifier.height(64.dp))
-            IconButton(onClick = { grant() }) {
-                Icon(Icons.Rounded.Settings, "Open settings", modifier = Modifier.size(32.dp))
             }
         }
     }
---
+++ app/src/main/java/com/numq/stash/permission/PermissionsRequired.kt
@@ -0,0 +1,24 @@
+package com.numq.stash.permission
+
+import androidx.compose.runtime.Composable
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import kotlinx.coroutines.runBlocking
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun PermissionsRequired(permissions: List<String>, content: @Composable () -> Unit) {
+
+    val permissionsState = rememberMultiplePermissionsState(permissions)
+
+    if (permissionsState.allPermissionsGranted) {
+        content()
+    } else {
+        PermissionsDenied(permissionsState.permissions) {
+            runBlocking {
+                permissionsState.launchMultiplePermissionRequest()
+            }
+        }
+    }
+
+}
